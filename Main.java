a8a3104659e1e8aa654ba589cb3fcd4ebc4992a4
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-06 22:59:50 -0600

    Menu V.2.0

--- Main.java
+++ Main.java
@@ -43,5 +43,71 @@ public class Main {
 		}catch(IOException e) {
 			System.out.println("Ha ocurrido una excepcion de tipo IO: "+e);
 		}
+		
+		boolean salir = false;
+		while(!salir) {
+			int opt =0;
+			System.out.println("Ingrese la accion a realizar");
+			System.out.println("1. Agregar producto");
+			System.out.println("2. Mostrar categoria");
+			System.out.println("3. Mostrar carrito");
+			System.out.println("4. Mostrar carrito ordenado");
+			System.out.println("5. Mostrar inventario");
+			System.out.println("6. Mostrar inventario ordenado");
+			opt = Integer.parseInt(scan.nextLine());
+			switch (opt) {
+			case 1:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println("Ingrese el nombre del producto:");
+				String product = scan.nextLine();
+				time_initLong = System.currentTimeMillis();
+				tienda.cart_add(product);
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 2:
+				System.out.println("------------------------------------------------------------------------");
+				System.out.println("Ingrese el nombre del producto:");
+				product = scan.nextLine();
+				time_initLong = System.currentTimeMillis();
+				System.out.println("El producto pertenece a: "+tienda.show_category(product));
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 3:
+				System.out.println("------------------------------------------------------------------------");
+				time_initLong = System.currentTimeMillis();
+				System.out.println(tienda.show_cart_unsorted());
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 4:
+				System.out.println("------------------------------------------------------------------------");
+				time_initLong = System.currentTimeMillis();
+				System.out.println(tienda.show_cart_sorted());
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 5:
+				System.out.println("------------------------------------------------------------------------");
+				time_initLong = System.currentTimeMillis();
+				System.out.println(tienda.inv_show());
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			case 6:
+				System.out.println("------------------------------------------------------------------------");
+				time_initLong = System.currentTimeMillis();
+				System.out.println(tienda.inv_show_sorted());
+				System.out.println(System.currentTimeMillis()-time_initLong+"ms");
+				System.out.println("------------------------------------------------------------------------");
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opt);
+			}
+			
+		}
+
+	}
 
 }
