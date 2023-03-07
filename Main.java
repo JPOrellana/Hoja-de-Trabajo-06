2f2aabcde6896351a5ae7a8f2c426cec550bb393
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-03-06 22:58:41 -0600

    Menu V.1.0

---
+++ Main.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+import java.awt.LinearGradientPaint;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * @author Andres
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nMap =1;
+		Scanner scan = new Scanner(System.in);
+		long time_initLong;
+		boolean bandera1 = true;
+		while(bandera1) {
+			System.out.println("Ingrese el tipo de mapa a utilizar:");
+			System.out.println("1. HashMap");
+			System.out.println("2. LinkedHashMap");
+			System.out.println("3. TreeMap");
+			nMap = Integer.parseInt(scan.nextLine());
+			if (nMap==1 || nMap == 2 || nMap == 3) {
+				bandera1 = false;
+			}
+		}
+		Tienda tienda = new Tienda(nMap);
+		try {
+			FileReader r = new FileReader("ListadoProducto.txt");
+			BufferedReader br = new BufferedReader(r);
+			String line;
+			while((line=br.readLine())!=null){
+				String[] values = line.split("\\|");
+				tienda.inv_add(values[1], values[0]);
+			}
+			br.close();
+		}catch(IOException e) {
+			System.out.println("Ha ocurrido una excepcion de tipo IO: "+e);
+		}
+
+}
